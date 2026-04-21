ecc421f57aa9a611e73b3e9df88890965c58e521
sugarchips <babybellargyll@gmail.com>
2026-04-21 16:24:36 -0400

    Initial Setup

---
+++ src/main/kotlin/org/valkyrienskies/valkyrien_depths/client/ValkyrienDepthsModClient.kt
@@ -0,0 +1,12 @@
+package org.valkyrienskies.valkyrien_depths.client
+
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
+
+class ValkyrienDepthsModClient {
+    companion object {
+        @JvmStatic
+        fun clientInit(event: FMLClientSetupEvent) {
+            // Put anything initialized on forge-side client here.
+        }
+    }
+}
