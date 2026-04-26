7311ba9205700c602bb311d4217f9f9c1076f40a
sugar-chips <babybellargyll@gmail.com>
2026-04-26 14:20:02 -0400

    Floodgate textures and lang

--- src/main/kotlin/org/valkyrienskies/valkyrien_depths/blocks/FloodgateBlock.kt
+++ src/main/kotlin/org/valkyrienskies/valkyrien_depths/blocks/FloodgateBlock.kt
@@ -1,25 +1,29 @@
 package org.valkyrienskies.valkyrien_depths.blocks
 
 import net.minecraft.core.BlockPos
+import net.minecraft.core.Direction
 import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResult
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.context.BlockPlaceContext
 import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.Level
+import net.minecraft.world.level.LevelAccessor
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
-import net.minecraft.world.level.block.TrapDoorBlock
+import net.minecraft.world.level.block.SimpleWaterloggedBlock
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.StateDefinition
 import net.minecraft.world.level.block.state.properties.BlockStateProperties
+import net.minecraft.world.level.material.Fluid
+import net.minecraft.world.level.material.FluidState
 import net.minecraft.world.level.material.Fluids
 import net.minecraft.world.phys.BlockHitResult
 import net.minecraft.world.phys.shapes.CollisionContext
 import net.minecraft.world.phys.shapes.Shapes
 import net.minecraft.world.phys.shapes.VoxelShape
 
-object FloodgateBlock : Block(Properties.copy(Blocks.COPPER_BLOCK)) {
+object FloodgateBlock : Block(Properties.copy(Blocks.COPPER_BLOCK).noOcclusion()), SimpleWaterloggedBlock {
     val openedState = BlockStateProperties.OPEN
     val waterLogged = BlockStateProperties.WATERLOGGED
 
@@ -34,7 +38,8 @@ object FloodgateBlock : Block(Properties.copy(Blocks.COPPER_BLOCK)) {
     }
 
     override fun getStateForPlacement(context: BlockPlaceContext): BlockState? {
-        return this.defaultBlockState()
+        val fluidState = context.level.getFluidState(context.clickedPos)
+        return this.defaultBlockState().setValue(waterLogged, fluidState.type == Fluids.WATER)
     }
 
     override fun use(
@@ -45,9 +50,10 @@ object FloodgateBlock : Block(Properties.copy(Blocks.COPPER_BLOCK)) {
         hand: InteractionHand,
         hit: BlockHitResult
     ): InteractionResult {
-        val newState = state.cycle<Boolean?>(TrapDoorBlock.OPEN) as BlockState
+        var newState = state.cycle<Boolean?>(openedState) as BlockState
+        if (!newState.getValue(openedState)) newState = newState.setValue(waterLogged, false)
         level.setBlock(pos, newState, 2)
-        if (newState.getValue<Boolean?>(TrapDoorBlock.WATERLOGGED) as Boolean) {
+        if (newState.getValue(waterLogged) && !newState.getValue(openedState)) {
             level.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(level))
         }
 
@@ -64,5 +70,51 @@ object FloodgateBlock : Block(Properties.copy(Blocks.COPPER_BLOCK)) {
             return Shapes.empty()
         }
         return super.getCollisionShape(state, level, pos, context)
+    }
+
+    override fun getFluidState(state: BlockState): FluidState {
+        return if (state.getValue(waterLogged)) Fluids.WATER.getSource(false) else super.getFluidState(state)
+    }
+
+    override fun updateShape(
+        state: BlockState,
+        direction: Direction,
+        neighborState: BlockState,
+        level: LevelAccessor,
+        pos: BlockPos,
+        neighborPos: BlockPos
+    ): BlockState {
+        if (state.getValue(waterLogged)) {
+            level.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(level))
+        }
+        return super.updateShape(state, direction, neighborState, level, pos, neighborPos)
+    }
+
+    override fun canPlaceLiquid(
+        getter: BlockGetter,
+        pos: BlockPos,
+        state: BlockState,
+        fluid: Fluid
+    ): Boolean {
+        return state.getValue(openedState) && !state.getValue(waterLogged) && fluid == Fluids.WATER
+    }
+
+    override fun placeLiquid(level: LevelAccessor, pos: BlockPos, state: BlockState, fluidState: FluidState): Boolean {
+        if (!state.getValue(openedState) || state.getValue(waterLogged) || fluidState.type != Fluids.WATER) {
+            return false
+        }
+        if (!level.isClientSide) {
+            level.setBlock(pos, state.setValue(waterLogged, true), 3)
+            level.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(level))
+        }
+        return true
+    }
+
+    override fun canBeReplaced(state: BlockState, fluid: Fluid): Boolean {
+        return false
+    }
+
+    override fun isCollisionShapeFullBlock(state: BlockState, level: BlockGetter, pos: BlockPos): Boolean {
+        return true
     }
 }
